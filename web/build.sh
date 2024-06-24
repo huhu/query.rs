c3b8a7ef3a188fb499d692d4e2f25bab9df8e622
Folyd <lyshuhow@gmail.com>
2024-06-23 23:41:56 -0700

    Fix build error again

--- web/build.sh
+++ web/build.sh
@@ -5,12 +5,17 @@ set -e
 # ls templates directory then render html with minijinja
 TEMPLATES_DIR="templates"
 
+# check dist directory exists
+if [ ! -d "dist" ]; then
+    mkdir dist
+fi
+
 for file in "$TEMPLATES_DIR"/*; do
     filename=$(basename -- "$file")
     name="${filename%.*}"
 
-    minijinja-cli $TEMPLATES_DIR/${filename} data.yaml -o ${name}.html
-    mkdir dist && cp -r assets core css lib pages vendor main.js dist
+    minijinja-cli $TEMPLATES_DIR/${filename} data.yaml -o dist/${name}.html
+    cp -r assets core css lib pages vendor main.js dist
 
     # Check if the command was successful
     if [ $? -ne 0 ]; then
