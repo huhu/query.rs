55d495e0c6a575cd51a1aa12793c90628b7248ee
Folyd <lyshuhow@gmail.com>
2024-07-02 22:17:49 -0700

    Svelte initial

---
+++ src/lib/search/index.js
@@ -0,0 +1,19 @@
+import CrateSearch from "./crate.js";
+import CaniuseSearch from "./caniuse.js";
+import BookSearch from "./book.js";
+import LintSearch from "./lint.js";
+import AttributeSearch from "./attribute.js";
+import DocSearch from "./docs/base.js";
+import CrateDocSearch from "./docs/crate-doc.js";
+import DescShardManager from "./docs/desc-shard.js";
+
+export {
+    CrateSearch,
+    CaniuseSearch,
+    BookSearch,
+    LintSearch,
+    AttributeSearch,
+    DocSearch,
+    CrateDocSearch,
+    DescShardManager,
+}
