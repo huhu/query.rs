5807489cb60372af4f48e65145258d752f26959e
Folyd <lyshuhow@gmail.com>
2024-07-09 18:54:38 -0700

    Adjust lib directory

--- svelte.config.js
+++ svelte.config.js
@@ -1,35 +1,21 @@
-import staticAdapter from '@sveltejs/adapter-static';
 import cloudflareAdapter from '@sveltejs/adapter-cloudflare';
 
-let config;
-
-if (process.env.EXTENSION_MODE === 'true') {
-    config = {
-        kit: {
-            adapter: staticAdapter({
-                // default options are shown. On some platforms
-                // these options are set automatically — see below
-                fallback: "index.html",
-            })
-        }
-    };
-} else {
-    config = {
-        kit: {
-            adapter: cloudflareAdapter({
-                routes: {
-                    include: ['/*'],
-                    exclude: ['<all>']
-                },
-                platformProxy: {
-                    configPath: 'wrangler.toml',
-                    environment: undefined,
-                    experimentalJsonConfig: false,
-                    persist: false
-                }
-            })
-        }
-    };
-}
-
-export default config;
+export default {
+    kit: {
+        adapter: cloudflareAdapter({
+            routes: {
+                include: ['/*'],
+                exclude: ['<all>']
+            },
+            platformProxy: {
+                configPath: 'wrangler.toml',
+                environment: undefined,
+                experimentalJsonConfig: false,
+                persist: false
+            }
+        }),
+        files: {
+            lib: "lib",
+        },
+    }
+};
