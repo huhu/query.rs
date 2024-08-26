58fe76161273d2020c5d4013faff8c45f42a0f40
Folyd <lyshuhow@gmail.com>
2024-08-27 01:15:34 +0800

    Don't import IndexManager in desc-shards.js to reduce bundle size (#19)

--- src/routes/suggest/[query]/+server.js
+++ src/routes/suggest/[query]/+server.js
@@ -1,6 +1,6 @@
 import stdDescShards from 'querylib/index/desc-shards/std';
 import searchIndex from 'querylib/index/std-docs.js';
-import { DocSearch } from 'querylib/search/index.js';
+import { DescShardManager, DocSearch } from 'querylib/search/index.js';
 
 /**
  * This function behaves as a suggestion provider for the search queries.
@@ -9,7 +9,7 @@ import { DocSearch } from 'querylib/search/index.js';
  * Format of the response is OpenSearch Suggestions 1.1.
  * https://github.com/dewitt/opensearch/blob/master/mediawiki/Specifications/OpenSearch/Extensions/Suggestions/1.1/Draft%201.wiki
  */
-export async function GET({params, platform}) {
+export async function GET({ params }) {
     let query = params.query;
 
     let result = [];
@@ -27,8 +27,8 @@ export async function GET({params, platform}) {
      * @type {string[]}
      * */
     let urls = [];
-    
-    await stdSearch(query, completions, desc, urls, 4);
+
+    await stdSearch(query, completions, desc, urls, 8);
     // await crateSearch(query, completions, desc, urls, 3);
 
     result.push(completions);
@@ -49,7 +49,7 @@ export async function GET({params, platform}) {
  * @param {number} maxCount 
  */
 async function stdSearch(query, completions, desc, urls, maxCount) {
-    const descShards = await StdShardManager.create();
+    const descShards = await DescShardManager.create(stdDescShards);
     let stdSearcher = new DocSearch(
         "std",
         structuredClone(searchIndex),
@@ -102,38 +102,3 @@ async function stdSearch(query, completions, desc, urls, maxCount) {
 //         urls.push("https://docs.rs/" + entry["id"]);
 //     }
 // }
-
-
-class StdShardManager {
-    constructor() {
-        // A dummy descShards map to allow interact in librustdoc's DocSearch js
-        this.descShards = new DummyMap();
-        // The real crate -> desc shard map.
-        this._descShards = new Map();
-    }
-
-    static async create() {
-        const shardManager = new StdShardManager();
-        shardManager.addCrateDescShards();
-        return shardManager;
-    }
-
-    addCrateDescShards() {
-        const descShards = stdDescShards;
-        this._descShards = new Map([...this._descShards, ...descShards]);
-    }
-
-    // Load a single desc shard.
-    // Compatible with librustdoc main.js.
-    async loadDesc({ descShard, descIndex }) {
-        let crateDescShard = this._descShards.get(descShard.crate);
-        if (!crateDescShard || crateDescShard.length === 0) {
-            return null;
-        }
-        return crateDescShard[descShard.shard][descIndex];
-    }
-}
-
-class DummyMap {
-    set(_ke, _value) { }
-}
