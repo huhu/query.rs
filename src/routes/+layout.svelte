ec3f768e2d93de9da3abf3d0f4886c2e5bf8b769
Folyd <lyshuhow@gmail.com>
2025-01-02 23:18:53 +0800

    Show logo for reddit page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,5 @@
 <script>
+  import { page } from "$app/stores";
   import "../app.css";
 
   const menus = [
@@ -19,15 +20,25 @@
 
 <div class="flex-layout" style="flex-direction: column;">
   <div class="max-w-[1160px] w-full flex flex-col">
-    <ul
-      class="hidden list-none flex-row overflow-auto md:flex self-end py-5 text-base md:text-xl"
-    >
-      {#each menus as menu}
-        <li class="px-5 rounded-md hover:bg-[#f9bc2d46]">
-          <a href={menu.path}>{menu.name}</a>
-        </li>
-      {/each}
-    </ul>
+    <div class="flex flex-row justify-between items-center">
+      <a href="/"
+        ><img
+          src="/assets/logo.svg"
+          alt="logo"
+          class="w-32 mx-6"
+          class:hidden={$page.url.pathname !== "/reddit"}
+        />
+      </a>
+      <ul
+        class="hidden list-none flex-row overflow-auto md:flex self-end py-5 text-base md:text-xl"
+      >
+        {#each menus as menu}
+          <li class="px-5 rounded-md hover:bg-[#f9bc2d46]">
+            <a href={menu.path}>{menu.name}</a>
+          </li>
+        {/each}
+      </ul>
+    </div>
     <slot />
   </div>
   <footer class="pb-6 text-center px-4">
